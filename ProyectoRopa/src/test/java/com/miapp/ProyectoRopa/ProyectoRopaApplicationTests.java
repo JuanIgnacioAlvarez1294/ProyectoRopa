2e61c95f58567f714dd07e4683a325ef4bfab8c2
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2024-09-11 13:49:40 -0300

    Initial commit: Proyecto Spring Boot creado con Web, JPA y MySQL

---
+++ ProyectoRopa/src/test/java/com/miapp/ProyectoRopa/ProyectoRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miapp.ProyectoRopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
