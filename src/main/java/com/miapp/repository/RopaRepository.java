6e63d7fbacc44778cca1ac39b6dc83e690514cf1
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2024-09-11 14:10:46 -0300

    Creación de RopaRepository que extiende de JpaRepository

---
+++ src/main/java/com/miapp/repository/RopaRepository.java
@@ -0,0 +1,7 @@
+package com.miapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.miapp.model.Ropa;
+
+public interface RopaRepository extends JpaRepository<Ropa, Long> {
+}
